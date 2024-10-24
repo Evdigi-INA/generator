f11f63a5cf9028e9e0cec1a30398650f521a11d7
Zzzul <mohammad.zulfahmi03@gmail.com>
2024-10-24 16:40:06 +0700

    #66 | fix image service class and model stub :hammer:

--- src/Generators/ModelGenerator.php
+++ src/Generators/ModelGenerator.php
@@ -37,39 +37,39 @@ class ModelGenerator
 
         foreach ($request['fields'] as $i => $field) {
             $fields .= match ($i + 1 != $totalFields) {
-                true => "'" . str()->snake($field) . "', ",
-                default => "'" . str()->snake($field) . "']",
+                true => "'" . str()->snake($field)->toString() . "', ",
+                default => "'" . str()->snake($field)->toString() . "']",
             };
 
             if ($request['input_types'][$i] == 'password') {
-                $protectedHidden .= "'" . str()->snake($field) . "', ";
+                $protectedHidden .= "'" . str()->snake($field)->toString() . "', ";
             }
 
             switch ($request['column_types'][$i]) {
                 case 'date':
                     if ($request['input_types'][$i] != 'month') {
                         $dateFormat = config('generator.format.date') ?? 'd/m/Y';
-                        $casts .= "'" . str()->snake($field) . "' => 'date:$dateFormat', ";
+                        $casts .= "'" . str()->snake($field)->toString() . "' => 'date:$dateFormat', ";
                     }
                     break;
                 case 'time':
                     $timeFormat = config('generator.format.time') ? config('generator.format.time') : 'H:i';
-                    $casts .= "'" . str()->snake($field) . "' => 'datetime:$timeFormat', ";
+                    $casts .= "'" . str()->snake($field)->toString() . "' => 'datetime:$timeFormat', ";
                     break;
                 case 'year':
-                    $casts .= "'" . str()->snake($field) . "' => 'integer', ";
+                    $casts .= "'" . str()->snake($field)->toString() . "' => 'integer', ";
                     break;
                 case 'dateTime':
-                    $casts .= "'" . str()->snake($field) . "' => 'datetime:$dateTimeFormat', ";
+                    $casts .= "'" . str()->snake($field)->toString() . "' => 'datetime:$dateTimeFormat', ";
                     break;
                 case 'float':
-                    $casts .= "'" . str()->snake($field) . "' => 'float', ";
+                    $casts .= "'" . str()->snake($field)->toString() . "' => 'float', ";
                     break;
                 case 'boolean':
-                    $casts .= "'" . str()->snake($field) . "' => 'boolean', ";
+                    $casts .= "'" . str()->snake($field)->toString() . "' => 'boolean', ";
                     break;
                 case 'double':
-                    $casts .= "'" . str()->snake($field) . "' => 'double', ";
+                    $casts .= "'" . str()->snake($field)->toString() . "' => 'double', ";
                     break;
                 case 'foreignId':
                     $constrainPath = GeneratorUtils::getModelLocation($request['constrains'][$i]);
@@ -108,35 +108,37 @@ class ModelGenerator
             switch ($request['input_types'][$i]) {
                 case 'month':
                     $castFormat = config('generator.format.month') ? config('generator.format.month') : 'm/Y';
-                    $casts .= "'" . str()->snake($field) . "' => 'date:$castFormat', ";
+                    $casts .= "'" . str()->snake($field)->toString() . "' => 'date:$castFormat', ";
                     break;
                 case 'week':
-                    $casts .= "'" . str()->snake($field) . "' => 'date:Y-\WW', ";
+                    $casts .= "'" . str()->snake($field)->toString() . "' => 'date:Y-\WW', ";
                     break;
                 case 'file':
                     // $uploadPaths .= "public string $" . GeneratorUtils::singularCamelCase($request['fields'][$i]) . "Path = '" . GeneratorUtils::pluralKebabCase($request['fields'][$i]) . "', ";
 
-                    $setReturnComment = $this->setReturnComment(config(key: 'generator.image.disk', default: 'storage.public'));
+                    // $setReturnComment = $this->setReturnComment(config(key: 'generator.image.disk', default: 'storage.public'));
 
                     $castImages .= GeneratorUtils::replaceStub(replaces: [
                         'fieldCamelCase' => str($field)->camel()->toString(),
                         'path' => GeneratorUtils::pluralKebabCase($field),
                         'disk' => config(key: 'generator.image.disk', default: 'storage.local'),
                         'defaultImage' => config(key: 'generator.image.default', default: 'https://via.placeholder.com/350?text=No+Image+Avaiable'),
-                        'returnPublicPath' => $setReturnComment['public_path'],
-                        'returnStoragePublicS3' => $setReturnComment['storage_public_s3'],
-                        'returnStorageLocal' => $setReturnComment['storage_local'],
-                    ], stubName: 'model-cast') . "\t";
+                        // 'returnPublicPath' => $setReturnComment['public_path'],
+                        // 'returnStoragePublicS3' => $setReturnComment['storage_public_s3'],
+                        // 'returnStorageLocal' => $setReturnComment['storage_local'],
+                        'fieldSnakeCase' => str($field)->snake()->toString(),
+                        'fieldPascalCase' => GeneratorUtils::pascalCase($field)
+                    ], stubName: 'model-cast') . "\n\t";
                     break;
             }
 
             // integer/bigInteger/tinyInteger/
             if (str_contains(haystack: $request['column_types'][$i], needle: 'integer')) {
-                $casts .= "'" . str()->snake($field) . "' => 'integer', ";
+                $casts .= "'" . str()->snake($field)->toString() . "' => 'integer', ";
             }
 
             if (in_array(needle: $request['column_types'][$i], haystack: ['string', 'text', 'char']) && $request['input_types'][$i] != 'week' && $request['input_types'][$i] != 'file') {
-                $casts .= "'" . str()->snake($field) . "' => 'string', ";
+                $casts .= "'" . str()->snake($field)->toString() . "' => 'string', ";
             }
         }
 
--- stubs/generators/publish/utils/Services/ImageServiceV2.php
+++ stubs/generators/publish/utils/Services/ImageServiceV2.php
@@ -3,181 +3,228 @@
 namespace App\Generators\Services;
 
 use Illuminate\Http\UploadedFile;
+use App\Generators\ImageUploadOption;
 use Illuminate\Support\Facades\Storage;
 use App\Generators\Interfaces\ImageServiceInterfaceV2;
-use Intervention\Image\Interfaces\EncodedImageInterface;
 
 class ImageServiceV2 implements ImageServiceInterfaceV2
 {
     /**
-     * Upload an image to disk and return the image path.
+     * Upload an image to disk and return the image name.
      */
     public function upload(
         string $name,
         string $path,
         ?string $defaultImage = null,
-        ?string $disk = null,
-        ?int $width = null,
-        ?int $height = null,
-        ?bool $crop = null,
-        ?bool $aspectRatio = null,
-        ?bool $isCustomUpload = null
+        string $disk = 'storage.public',
+        int $width = 300,
+        int $height = 300,
+        bool $crop = true,
+        bool $aspectRatio = true,
+        bool $isCustomUpload = false
     ): ?string {
         $file = request()->file(key: $name);
 
         if (!$file || !$file->isValid()) {
-            return $this->getActualImageName(name: $defaultImage);
+            return $this->getActualImageName(name: $defaultImage, path: $path, disk: $disk);
         }
 
         if ($isCustomUpload) {
-            // Implement custom upload logic if needed
-            return $this->getActualImageName(name: $defaultImage);
+            // TODO: Write your custom image upload logic
+            return $this->getActualImageName(name: $defaultImage, path: $path, disk: $disk);
         }
 
-        return $this->handleFileUpload(options: [
-            'file' => $file,
-            'path' => "/$path/",
-            'default_image' => $defaultImage,
-            'disk' => $disk ?? config(key: 'generator.image.disk', default: 'storage.public'),
-            'width' => $width ?? config(key: 'generator.image.width', default: 300),
-            'height' => $height ?? config(key: 'generator.image.height', default: 300),
-            'crop' => $crop ?? config(key: 'generator.image.crop', default: true),
-            'aspect_ratio' => $aspectRatio ?? config(key: 'generator.image.aspect_ratio', default: true),
-        ]);
+        $options = new ImageUploadOption(
+            file: $file,
+            path: "/$path/",
+            defaultImage: $defaultImage,
+            disk: $disk ?? config(key: 'generator.image.disk', default: 'storage.public'),
+            width: $width ?? config(key: 'generator.image.width', default: 300),
+            height: $height ?? config(key: 'generator.image.height', default: 300),
+            crop: $crop,
+            aspectRatio: $aspectRatio
+        );
+
+        return $this->handleFileUpload($options);
     }
 
     /**
-     * Handles the file upload process with optional image manipulation using Intervention/Image.
-     *
-     * @param array $options {
-     *     An associative array of options for handling file upload.
-     *
-     *     @type string $name The name of the file input field.
-     *     @type string $path The path where the image should be stored.
-     *     @type string|null $defaultImage The default image to return if no file is uploaded.
-     *     @type string|null $disk The storage disk to use (e.g., 'public', 's3', 'storage.local', 'storage.public'). Defaults to 'storage.public'.
-     *     @type int $width The width to resize the image to. Defaults to 300.
-     *     @type int $height The height to resize the image to. Defaults to 300.
-     *     @type bool|null $crop Whether to crop the image. Defaults to config setting.
-     *     @type bool|null $isCustomUpload Whether to handle custom upload logic. Defaults to false.
-     *     @type UploadedFile $file The uploaded file object.
-     * }
-     *
-     * @return string The filename of the uploaded or processed image.
+     * Deletes an image from the specified disk.
      */
-    private function handleFileUpload(array $options): string
+    public function delete(string $path, ?string $image, string $disk = 'storage.local'): bool
     {
-        $filename = $this->generateFilename(file: $options['file']);
-        $setDiskName = $this->setDiskName(disk: $options['disk']);
-        $fullPath = $options['path'] . '/' . $filename;
+        if (!$image) {
+            return false;
+        }
+
+        $imageName = $this->getActualImageName(name: $image, path: $path, disk: $disk);
+        $fullPath = "$path/$imageName";
+        $actualDisk = $this->setDiskName($disk);
+
+        return match ($actualDisk) {
+            's3', 'public', 'local' => Storage::disk(name: $actualDisk)->delete(paths: $fullPath),
+            default => @unlink(filename: public_path(path: $fullPath)),
+        };
+    }
+
+    /**
+     * Converts a disk name alias to its actual name.
+     *
+     * This method accepts a disk name alias and returns its actual name.
+     * The supported aliases are 's3', 'storage.public' same as 'public' and
+     * 'storage.local' same for 'local'. If no alias is provided, the method
+     * defaults to 'public_path'.
+     */
+    public function setDiskName(string $disk): string
+    {
+        return match ($disk) {
+            's3', 'local', 'public' => $disk,
+            'storage.public' => 'public',
+            'storage.local' => 'local',
+            default => 'public_path',
+        };
+    }
+
+    /**
+     * Returns the default image URL to be used as a placeholder for non-existent
+     * images. The default placeholder image is a 300x300 image with text
+     * "No Image Available" from placehold.co.
+     */
+    public function getPlaceholderImage(): string
+    {
+        return config(key: 'generator.image.default', default: 'https://placehold.co/300?text=No+Image+Available');
+    }
+
+    /**
+     * Determines if the specified disk is a private S3 disk.
+     */
+    public function isPrivateS3(string $disk): bool
+    {
+        return $disk === 's3' && config(key: 'filesystems.disks.s3.visibility') !== 'public';
+    }
+
+    /**
+     * Generates a temporary URL to a file on the specified disk.
+     *
+     * The URL is valid for 5 minutes.
+     */
+    public function getTemporaryUrl(string $disk, string $path, string $value): string
+    {
+        return Storage::disk(name: $disk)->temporaryUrl(path: "$path/$value", expiration: now()->addMinutes(value: 5));
+    }
+
+    /**
+     * Returns a publicly accessible URL to a file on the specified disk.
+     *
+     * This is the URL that will be used in the `<img>` tag to display the image.
+     */
+    public function getPublicUrl(string $disk, string $path, string $value): string
+    {
+        return Storage::disk(name: $disk)->url(path: "$path/$value");
+    }
+
+    /**
+     * Returns a publicly accessible URL to a local file.
+     *
+     * This is the URL that will be used in the `<img>` tag to display the image.
+     * The URL is generated using the Laravel `asset` helper.
+     */
+    public function getLocalAssetUrl(string $path, string $value): string
+    {
+        return asset(path: "$path/$value");
+    }
+
+    /**
+     * Handles the file upload process with optional image manipulation.
+     */
+    private function handleFileUpload(ImageUploadOption $options): string
+    {
+        $filename = $this->generateFilename($options->file);
+        $disk = $this->setDiskName($options->disk);
 
         if (!$this->isInterventionAvailable()) {
-            // upload to public_path()
-            if (!in_array(needle: $options['disk'], haystack: ['s3', 'storage.local', 'storage', 'storage.public'])) {
-                $this->moveToPublicFolder(file: $options['file'], path: $options['path'], filename: $filename);
-
-                $this->deleteOldImage(options: $options);
-
-                return $filename;
-            }
-
-            Storage::disk($setDiskName)->put($fullPath, $options['file']);
-
-            $this->deleteOldImage(options: $options);
-
-            return $filename;
+            return $this->handleWithoutIntervention(options: $options, filename: $filename, disk: $disk);
         }
 
-        $image = $this->processWithInterventionImage(options: $options);
+        return $this->handleWithIntervention(options: $options, filename: $filename, disk: $disk);
+    }
 
-        switch ($options['disk']) {
-            case 'public':
-                // upload to public_path()
-                $this->moveToPublicFolder(file: $options['file'], path: $options['path'], filename: $filename);
-                break;
-            default:
-                Storage::disk($setDiskName)->put($fullPath, $image);
-                break;
-        }
+    /**
+     * Handles the file upload process without using the Intervention Image library.
+     *
+     * This method stores the uploaded file directly to the specified disk and deletes any old image if present.
+     */
+    private function handleWithoutIntervention(ImageUploadOption $options, string $filename, string $disk): string
+    {
+        Storage::disk(name: $this->setDiskName($disk))->putFileAs(path: $options->path, file: $options->file, name: $filename);
 
-        $this->deleteOldImage(options: $options);
+        $this->deleteOldImage($options);
 
         return $filename;
     }
 
     /**
-     * Maps a given disk name to a standardized name to be used when interacting with Storage.
+     * Handles the file upload process with optional image manipulation when the Intervention Image library is available.
      */
-    public function setDiskName(string $disk): string
+    private function handleWithIntervention(ImageUploadOption $options, string $filename, string $disk): string
     {
-        return match ($disk) {
-            's3' => 's3',
-            'storage.public' => 'public',
-            'storage' => 'public',
-            'storage.local' => 'local',
-            default => $disk,
-        };
+        $image = $this->processWithInterventionImage($options);
+
+        Storage::disk($this->setDiskName($disk))->put(path: "$options->path/$filename", contents: (string) $image);
+
+        $this->deleteOldImage($options);
+
+        return $filename;
     }
 
     /**
-     * Deletes an old image from the specified disk if a default image is provided in the options.
+     * Deletes the old image if a default image is specified in the options.
+     *
+     * This method checks for the presence of a default image in the provided
+     * options array and deletes it from the specified disk and path if found.
      */
-    private function deleteOldImage(array $options): bool
+    private function deleteOldImage(ImageUploadOption $options): bool
     {
-        if ($options['default_image']) {
-            return $this->delete(path: $options['path'], image: $options['default_image'], disk: $options['disk']);
-        }
-
-        return false;
+        return $options->defaultImage ? $this->delete($options->path, $options->defaultImage, $options->disk) : false;
     }
 
     /**
-     * Move the uploaded file to the public folder.
-     */
-    private function moveToPublicFolder(UploadedFile $file, string $path, string $filename): bool
-    {
-        $file->move(directory: public_path($path), name: $filename);
-
-        if (file_exists(filename: public_path("$path$filename"))) {
-            return true;
-        }
-
-        return false;
-    }
-
-    /**
-     * Generate a filename for the uploaded image.
+     * Generates a random filename for an uploaded image file.
+     *
+     * If the Intervention Image library is available, a random string will be
+     * generated and appended with the '.webp' extension. Otherwise, the
+     * {@link Illuminate\Http\UploadedFile::hashName()} method will be used.
      */
     private function generateFilename(UploadedFile $file): string
     {
-        return $this->isInterventionAvailable()
-            ? str()->random(30) . '.webp'
-            : $file->hashName();
+        return $this->isInterventionAvailable() ? str()->random(30) . '.webp' : $file->hashName();
     }
 
     /**
-     * Process the image using Intervention/Image if available.
+     * Processes an image using the Intervention Image library.
+     *
+     * This method reads an image file and encodes it to the WebP format with
+     * a specified quality. It optionally crops and resizes the image based on
+     * the provided options.
      */
-    private function processWithInterventionImage(array $options): EncodedImageInterface
+    private function processWithInterventionImage(ImageUploadOption $options): \Intervention\Image\Interfaces\EncodedImageInterface
     {
-        $image = \Intervention\Image\Laravel\Facades\Image::read($options['file']);
+        $image = \Intervention\Image\Laravel\Facades\Image::read($options->file);
         $encode = new \Intervention\Image\Encoders\WebpEncoder(65);
 
-        if ($options['crop']) {
-            // constraint to maintain aspect ratio
-            if ($options['aspect_ratio']) {
-                return $image->scaleDown($options['width'], $options['height'])->encode($encode);
-            }
-
-            return $image->resizeDown($options['width'], $options['height'])->encode($encode);
-        }
-
-        return $image->encode($encode);
+        return $options->crop
+            ? ($options->aspectRatio
+            ? $image->scaleDown($options->width, $options->height)->encode($encode)
+            : $image->resizeDown($options->width, $options->height)->encode($encode))
+            : $image->encode($encode);
     }
 
-
     /**
-     * Check if Intervention/Image is available.
+     * Checks if the Intervention Image library is available.
+     *
+     * This method verifies the existence of the Intervention Image library
+     * by checking if the corresponding class is available. It is used to
+     * determine if image manipulation capabilities are supported.
      */
     private function isInterventionAvailable(): bool
     {
@@ -185,46 +232,32 @@ class ImageServiceV2 implements ImageServiceInterfaceV2
     }
 
     /**
-     * Delete an image from the specified disk.
+     * Retrieves the actual image name if it exists on the specified disk.
+     *
+     * This method extracts the image name from the given path, cleans up any query
+     * parameters, and checks if the image exists on the specified disk. If the image
+     * does not exist or if no name is provided, it returns null.
      */
-    public function delete(string $path, ?string $image, string $disk = 'local'): bool
+    protected function getActualImageName(string $name = null, string $path, string $disk = 'public'): ?string
     {
-        if (!$image) {
-            return false;
+        if (!$name) {
+            return null;
         }
 
-        $imageName = $this->getActualImageName(name: $image);
-        $fullPath = "$path/$imageName";
+        $image = last(array: explode(separator: '/', string: $name));
+        $cleanImg = str_contains(haystack: $image, needle: '?expires') ? str(string: $image)->before(search: '?expires')->toString() : $image;
+        $fullPath = "$path/$cleanImg";
+        $actualDisk = $this->setDiskName($disk);
 
-        return match ($disk) {
-            'public' => @unlink(filename: $fullPath),
-            'storage.public' => Storage::disk(name: 'public')->delete(paths: $fullPath),
-            'storage' => Storage::disk(name: 'public')->delete(paths: $fullPath),
-            'storage.local' => Storage::disk(name: 'local')->delete(paths: $fullPath),
-            default => Storage::disk(name: 's3')->delete(paths: $fullPath),
+        $exists = match ($actualDisk) {
+            's3', 'local', 'public' => Storage::disk(name: $actualDisk)->exists(path: $fullPath),
+            default => file_exists(filename: public_path($fullPath)),
         };
-    }
 
-    /**
-     * Gets the actual image name by removing any '/' or '?' in the image name.
-     */
-    protected function getActualImageName(?string $name = null): ?string
-    {
-        if(!$name) {
+        if (!$exists) {
             return null;
         }
 
-        $arrImg = explode(separator: '/', string: $name);
-        $image = $arrImg[count(value: $arrImg) - 1];
-
-        if (!str_contains(needle: '.webp', haystack: $image)) {
-            return null;
-        }
-
-        if (str_contains(needle: '?expires', haystack: $image)) {
-            $image = str(string: $image)->before(search: '?expires')->toString();
-        }
-
-        return $image;
+        return $cleanImg;
     }
 }
