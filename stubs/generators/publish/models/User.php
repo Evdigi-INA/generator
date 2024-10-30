61de1094bdcea1c5f8d21787df20b941808e1fbb
Zzzul <mohammad.zulfahmi03@gmail.com>
2024-10-30 11:51:31 +0700

    #66 | fix cast image in user model :hammer:

--- stubs/generators/publish/models/User.php
+++ stubs/generators/publish/models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use App\Generators\Services\ImageServiceV2;
 
 class User extends Authenticatable
 {
@@ -39,13 +41,51 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast.
+     * Get the attributes that should be cast.
      *
-     * @var array
+     * @return array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime:Y-m-d H:i',
-        'created_at' => 'datetime:Y-m-d H:i',
-        'updated_at' => 'datetime:Y-m-d H:i',
-    ];
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime:Y-m-d H:i',
+            'created_at' => 'datetime:Y-m-d H:i',
+            'updated_at' => 'datetime:Y-m-d H:i',
+        ];
+    }
+
+    /**
+     * Accessor for the 'avatar' attribute.
+     */
+    protected function avatar(): Attribute
+    {
+        return Attribute::make(
+            get: fn(?string $value): string => $this->getAvatarUrl(value: $value)
+        );
+    }
+
+    /**
+     * Returns the URL of an 'avatar'.
+     *
+     * If the avatar does not exist, it returns a placeholder avatar.
+     * If the avatar exists, it returns a temporary URL to the avatar on the local disk,
+     * or a publicly accessible URL to the avatar on the S3 or public disk.
+     * If the disk is neither local, S3, nor public, it returns a publicly accessible URL to the avatar on the local disk.
+     */
+    private function getAvatarUrl(?string $value): string
+    {
+        $path = 'avatars';
+        $imageService = new ImageServiceV2();
+        $disk = $imageService->setDiskName('storage.public');
+
+        if (!$value) {
+            return $imageService->getPlaceholderImage();
+        }
+
+        return match (true) {
+            $imageService->isPrivateS3(disk: $disk) || $disk === 'local' => $imageService->getTemporaryUrl(disk: $disk, image: "$path/$value"),
+            in_array(needle: $disk, haystack: ['s3', 'public']) => $imageService->getStoragePublicUrl(disk: $disk, image: "$path/$value"),
+            default => $imageService->getPublicAssetUrl(image: "$path/$value"),
+        };
+    }
 }
